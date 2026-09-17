c6340899693d9f5c962c1760723b014b2b247699
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-09-17 09:50:26 +0530

    ESLINT

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
-import {resolve} from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(import.meta.dirname, './src')
+      "@": resolve(import.meta.dirname, "./src"),
     },
   },
-})
+});
